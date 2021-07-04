aca7d70eb91de7e8f874bc3f94abb663a68c5230
QueGonJinn <molialow13@gmail.com>
2021-07-04 19:28:43 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {        
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?1`);;
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?1`);
     }
 }
 
